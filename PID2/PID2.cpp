63d14b58ad18680017a94f18370b00e86e4296fb
MartinL1 <31612615+MartinL1@users.noreply.github.com>
2018-04-24 08:52:23 +0100

    Update PID2.cpp

--- PID2/PID2.cpp
+++ PID2/PID2.cpp
@@ -32,64 +32,35 @@
 // Default constructor
 PID::PID() {}
 
-// P Controller constructor - with symmetrical PID output limits
-PID::PID(float kp, float pidLimit) : 
-	kp(kp), pidLowerLimit(-pidLimit), pidUpperLimit(pidLimit) {}
-
-// P Controller constructor - with defined lower and upper PID output limits 
-PID::PID(float kp, float pidLowerLimit, float pidUpperLimit) : 
-	kp(kp), pidLowerLimit(pidLowerLimit), pidUpperLimit(pidUpperLimit) {}
-
-// PID Controller constructor - with symmetrical integral and PID output limits
-PID::PID(float kp, float ki, float kd, float iLimit, float pidLimit) : 
-	kp(kp), ki(ki), kd(kd), iLowerLimit(-iLimit), iUpperLimit(iLimit), 
-	pidLowerLimit(-pidLimit), pidUpperLimit(pidLimit) {}
+// P Controller constructor - with defined minimum and maximum PID output limits 
+PID::PID(float kp, float pidMinLimit, float pidMaxLimit) : 
+	kp(kp), pidMinLimit(pidMinLimit), pidMaxLimit(pidMaxLimit) {}
 	
-// PID Controller constructor - with defined lower and upper integral and PID output limits
-PID::PID(float kp, float ki, float kd, float iLowerLimit, float iUpperLimit, 
-	float pidLowerLimit, float pidUpperLimit) : 
-	kp(kp), ki(ki), kd(kd), iLowerLimit(iLowerLimit), iUpperLimit(iUpperLimit), 
-	pidLowerLimit(pidLowerLimit), pidUpperLimit(pidUpperLimit) {}	
+// PID Controller constructor - with defined minimum and maximum integral and PID output limits
+PID::PID(float kp, float ki, float kd, float iMinLimit, float iMaxLimit, 
+	float pidMinLimit, float pidMaxLimit) : 
+	kp(kp), ki(ki), kd(kd), iMinLimit(iMinLimit), iMaxLimit(iMaxLimit), 
+	pidMinLimit(pidMinLimit), pidMaxLimit(pidMaxLimit) {}	
 
-// Initialise the P controller - with symmetrical PID output limits
-void PID::begin(float kp, float pidLimit)
+// Initialise the P controller - with defined minimum and maximum PID output limits 
+void PID::begin(float kp, float pidMinLimit, float pidMaxLimit)
 {
 	this->kp = kp;																								// Set the proportional P gain to the new value
-	this->pidLowerLimit = -pidLimit;															// Set the proportional P lower output limit
-	this->pidLowerLimit = pidLimit;																// Set the proportional P upper output limit
+	this->pidMinLimit = pidMinLimit;															// Set the proportional P minimum output limit
+	this->pidMaxLimit = pidMaxLimit;															// Set the proportional P maximum output limit
 }
-
-// Initialise the P controller - with defined lower and upper PID output limits 
-void PID::begin(float kp, float pidLowerLimit, float pidUpperLimit)
-{
-	this->kp = kp;																								// Set the proportional P gain to the new value
-	this->pidLowerLimit = pidLowerLimit;													// Set the proportional P lower output limit
-	this->pidUpperLimit = pidUpperLimit;													// Set the proportional P upper output limit
-}
-
-// Initialise the PID controller - with symmetrical integral and PID output limits
-void PID::begin(float kp, float ki, float kd, float iLimit, float pidLimit)
+	
+// Initialise the PID controller - with defined minimum and maximum integral and PID output limits
+void PID::begin(float kp, float ki, float kd, float iMinLimit, float iMaxLimit, 
+	float pidMinLimit, float pidMaxLimit)
 {
 	this->kp = kp;																								// Set the proportional P gain to the new value
 	this->ki = ki;																								// Set the integral I gain to the new value
 	this->kd = kd;																								// Set the derivative D gain to the new value
-	this->iLowerLimit = -iLimit;																	// Set the integral I lower output limit
-	this->iUpperLimit = iLimit;																		// Set the integral I upper output limit
-	this->pidLowerLimit = -pidLimit;															// Set the combined PID lower output limit
-	this->pidUpperLimit = pidLimit;																// Set the combined PID upper output limit
-}
-	
-// Initialise the PID controller - with defined lower and upper integral and PID output limits
-void PID::begin(float kp, float ki, float kd, float iLowerLimit, float iUpperLimit, 
-	float pidLowerLimit, float pidUpperLimit)
-{
-	this->kp = kp;																								// Set the proportional P gain to the new value
-	this->ki = ki;																								// Set the integral I gain to the new value
-	this->kd = kd;																								// Set the derivative D gain to the new value
-	this->iLowerLimit = iLowerLimit;															// Set the integral I lower output limit
-	this->iUpperLimit = iUpperLimit;															// Set the integral I upper output limit
-	this->pidLowerLimit = pidLowerLimit;													// Set the combined PID lower output limit
-	this->pidUpperLimit = pidUpperLimit;													// Set the combined PID upper output limit
+	this->iMinLimit = iMinLimit;																	// Set the integral I minimum output limit
+	this->iMaxLimit = iMaxLimit;																	// Set the integral I maximum output limit
+	this->pidMinLimit = pidMinLimit;															// Set the combined PID minimum output limit
+	this->pidMaxLimit = pidMaxLimit;															// Set the combined PID maximum output limit
 }	
 	
 // Calculate the classic P control loop
@@ -97,41 +68,41 @@ float PID::pCalculate(float setpoint, float input)
 {
   float error = setpoint - input;																// Calculate the error = setpoint - input
 	float pTerm = kp * error;                                 		// pTerm = (setpoint - input) * proportional gain 
-  pTerm = pTerm > pidUpperLimit ? pidUpperLimit : pTerm;    		// Check that the upper output limit is not exceeded
-  pTerm = pTerm < pidLowerLimit ? pidLowerLimit : pTerm;  			// Check that the lower output limit is not exceeded
+  pTerm = pTerm > pidMaxLimit ? pidMaxLimit : pTerm;    				// Check that the maximum output limit is not exceeded
+  pTerm = pTerm < pidMinLimit ? pidMinLimit : pTerm;  					// Check that the minimum output limit is not exceeded
   return pTerm;                                             		// Return the PID term
 }
 
-// Calculate the classic PID control loop - dt is the sample time in seconds
+// Calculate the classic PID control loop, (dt is the sample time in seconds)
 float PID::pidCalculate(float setpoint, float input, float dt)
 {
   float error = setpoint - input;																// Calculate the error = setpoint - input
 	float pTerm = kp * error;                                 		// pTerm = (setpoint - input) * proportional gain 
   iTerm += ki * error * dt;                                 		// iTerm += (setpoint - input) * integral gain * loop time (dt)
-  iTerm = iTerm > iUpperLimit ? iUpperLimit : iTerm;            // Check that the upper integral limit is not exceeded
-  iTerm = iTerm < iLowerLimit ? iLowerLimit : iTerm;            // Check that the lower integral limit is not exceeded
+  iTerm = iTerm > iMaxLimit ? iMaxLimit : iTerm;            		// Check that the maximum integral limit is not exceeded
+  iTerm = iTerm < iMinLimit ? iMinLimit : iTerm;            		// Check that the minimum integral limit is not exceeded
   float dTerm = kd * (error - prevError) / dt;              		// dTerm = derivative gain * (current error - previous error) / loop time (dt)
   float pidTerm = pTerm + iTerm + dTerm;                    		// Sum the PID terms
-  pidTerm = pidTerm > pidUpperLimit ? pidUpperLimit : pidTerm;  // Check that the upper output limit is not exceeded
-  pidTerm = pidTerm < pidLowerLimit ? pidLowerLimit : pidTerm;  // Check that the lower output limit is not exceeded
+  pidTerm = pidTerm > pidMaxLimit ? pidMaxLimit : pidTerm;  		// Check that the maximum output limit is not exceeded
+  pidTerm = pidTerm < pidMinLimit ? pidMinLimit : pidTerm;  		// Check that the minimum output limit is not exceeded
 	prevError = error;																						// Update the previous error value
   return pidTerm;                                           		// Return the PID term
 }
 
 // Calculate the "Derivative on Measurement" PID control loop with the input error (rather than error - prevError) as the derivative
-// This removes the "derivative kick" induced by a sudden change in the setpoint - dt is the sample time in seconds
+// This removes the "derivative kick" induced by a sudden change in the setpoint, (dt is the sample time in seconds)
 float PID::pidCalculateDOM(float setpoint, float input, float dt)
 {
   float error = setpoint - input;																// Calculate the error = setpoint - input
 	float pTerm = kp * error;                                 		// pTerm = (setpoint - input) * proportional gain 
   iTerm += ki * error * dt;                                 		// iTerm += (setpoint - input) * integral gain * loop time (dt)
-  iTerm = iTerm > iUpperLimit ? iUpperLimit : iTerm;            // Check that the upper integral limit is not exceeded
-  iTerm = iTerm < -iLowerLimit ? -iLowerLimit : iTerm;          // Check that the lower integral limit is not exceeded
+  iTerm = iTerm > iMaxLimit ? iMaxLimit : iTerm;            		// Check that the maximum integral limit is not exceeded
+  iTerm = iTerm < -iMinLimit ? -iMinLimit : iTerm;          		// Check that the minimum integral limit is not exceeded
   float inputError = input - prevInput;													// Calculate the input error = input - previous input
 	float dTerm = kd * -inputError / dt;                      		// dTerm = derivative gain * (-inputError) / loop time (dt)
   float pidTerm = pTerm + iTerm + dTerm;                    		// Sum the PID terms
-  pidTerm = pidTerm > pidUpperLimit ? pidUpperLimit : pidTerm;  // Check that the upper output limit is not exceeded
-  pidTerm = pidTerm < pidLowerLimit ? pidLowerLimit : pidTerm;  // Check that the lower output limit is not exceeded
+  pidTerm = pidTerm > pidMaxLimit ? pidMaxLimit : pidTerm;  		// Check that the maximum output limit is not exceeded
+  pidTerm = pidTerm < pidMinLimit ? pidMinLimit : pidTerm;  		// Check that the minimum output limit is not exceeded
   prevInput = input;																						// Update the previous input
 	return pidTerm;                                           		// Return the PID term
 }
@@ -140,36 +111,36 @@ void PID::setKp(float kp) { this->kp = kp; }										// Set the proportional P 
 void PID::setKi(float ki) { this->ki = ki; } 										// Set the integral I gain
 void PID::setKd(float kd) { this->kd = kd; }   									// Set the derivative D gain
 
-void PID::setILimit(float iLimit)
+void PID::setILimits(float iMinLimit, float iMaxLimit)
 {
-	this->iLowerLimit = -iLimit;																	// Set the integral I lower and upper limits to prevent integral wind-up
-	this->iUpperLimit = iLimit;
+	this->iMinLimit = iMinLimit;																	// Set the integral I minimum and maximum limits to prevent integral wind-up
+	this->iMaxLimit = iMaxLimit;
 }
 
-void PID::setPIDLimit(float pidLimit)
+void PID::setPIDLimits(float pidMinLimit, float pidMaxLimit)
 {
-	this->pidLowerLimit = -pidLimit;															// Set the PID controller lower and upper output limit
-	this->pidUpperLimit = pidLimit;
+	this->pidMinLimit = pidMinLimit;															// Set the PID controller minimum and maximum output limits
+	this->pidMaxLimit = pidMaxLimit;
 }
 
-void PID::setILowerLimit(float iLowerLimit)
+void PID::setIMinLimit(float iMinLimit)
 {
-	this->iLowerLimit = iLowerLimit;															// Set the integral I lower limit to prevent integral wind-up
+	this->iMinLimit = iMinLimit;																	// Set the integral I minimum limit to prevent integral wind-up
 }
 
-void PID::setIUpperLimit(float iUpperLimit)
+void PID::setIMaxLimit(float iMaxLimit)
 {
-	this->iUpperLimit = iUpperLimit;															// Set the integral I upper limit to prevent integral wind-up
+	this->iMaxLimit = iMaxLimit;																	// Set the integral I maximum limit to prevent integral wind-up
 }
 
-void PID::setPIDLowerLimit(float pidLowerLimit)
+void PID::setPIDMinLimit(float pidMinLimit)
 {
-	this->pidLowerLimit = pidLowerLimit;													// Set the PID controller lower output limit
+	this->pidMinLimit = pidMinLimit;															// Set the PID controller minimum output limit
 }
 
-void PID::setPIDUpperLimit(float pidUpperLimit)
+void PID::setPIDMaxLimit(float pidMaxLimit)
 {
-	this->pidUpperLimit = pidUpperLimit;													// Set the PID controller upper output limit
+	this->pidMaxLimit = pidMaxLimit;															// Set the PID controller maximum output limit
 }
 
 void PID::setPrevError(float prevError)
@@ -185,11 +156,11 @@ void PID::setPrevInput(float prevInput)
 float PID::getKp() { return kp; }																// Get the proportional P gain
 float PID::getKi() { return ki; }																// Get the integral I gain
 float PID::getKd()	{ return kd; }															// Get the derivative D gain
-float PID::getILimit() { return iUpperLimit; }									// Get the integral limits
-float PID::getPIDLimit() { return pidUpperLimit; }							// Get the PID output limits
-float PID::getILowerLimit() { return iLowerLimit; }							// Get the integral lower limit
-float PID::getIUpperLimit() { return iUpperLimit; }							// Get the integral upper limit
-float PID::getPIDLowerLimit() { return pidLowerLimit; }					// Get the PID output lower limit
-float PID::getPIDUpperLimit() { return pidUpperLimit; }					// Get the PID output upper limit
+float PID::getILimit() { return iMaxLimit; }										// Get the integral limits
+float PID::getPIDLimit() { return pidMaxLimit; }								// Get the PID output limits
+float PID::getIMinLimit() { return iMinLimit; }									// Get the integral minimum limit
+float PID::getIMaxLimit() { return iMaxLimit; }									// Get the integral maximum limit
+float PID::getPIDMinLimit() { return pidMinLimit; }							// Get the PID output minimum limit
+float PID::getPIDMaxLimit() { return pidMaxLimit; }							// Get the PID output maximum limit
 float PID::getPrevError() { return prevError; }									// Get the previous error value
 float PID::getPrevInput() { return prevInput; }									// Get the previous input value
